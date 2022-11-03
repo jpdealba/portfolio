e55dd01628d21fadc50bbc0e91befd7a54c0f825
jpdealba <yoda9@live.com.mx>
2022-11-03 07:03:04 -0600

    background color full screen

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
     setIsMuted(nextVal);
   };
   return (
-    <div className={`App ${darkTheme ? 'dark' : 'light'}`}>
+    <div className={`App ${darkTheme ? 'dark' : 'light'} bg-black h-screen`}>
       <div
         className="bg-backgroundLight dark:bg-background flex-col  ">
         <Mute toggleMute={toggleMute} isMuted={isMuted} />
